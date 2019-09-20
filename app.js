5907c67f62979c2c59e41e3f74a2c50415fe816e
dagibu <davidgiraldobueno@gmail.com>
2019-09-20 13:53:20 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOuput = document.querySelector('#total-expenses');
+
+let totalExpenses = 0 ;
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
+
+confirmBtn.addEventListener('click', () => {
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
+        return
+    }
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    expensesList.appendChild(newItem);
+
+    totalExpenses += +enteredAmount;
+    totalExpensesOuput.textContent = totalExpenses;
+    clear();
+});
+
+cancelBtn.addEventListener('click', clear );
